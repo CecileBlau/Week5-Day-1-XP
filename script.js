8a744e1a55399bf4de93dc063dc289de63ebdbb4
CecileBlau <92298805+CecileBlau@users.noreply.github.com>
2022-01-02 18:37:05 +0200

    xp

---
+++ script.js
@@ -0,0 +1,43 @@
+// let form = document.getElementById("form")
+
+// form.addEventListener("submit", functionSend)
+
+// function functionSend(event){
+// 	let name = document.getElementById("name").value;
+// 	let textArea= document.getElementById("textArea").value;
+// }
+
+//--------------------------------------------Exercise 3 : JSON Mario------------------------------------
+let marioGame = {
+	detail : "An amazing game!",
+	characters : {
+		mario : {
+			description:"Small and jumpy. Likes princesses.",
+			height: 10,
+			weight: 3,
+			speed: 12,
+		},
+		bowser : {
+			description: "Big and green, Hates princesses.",
+			height: 16,
+			weight: 6,
+			speed: 4,
+		},
+		princessPeach : {
+			description: "Beautiful princess.",
+			height: 12,
+			weight: 2,
+			speed: 2,
+		}
+	},
+}
+
+
+// Convert this JS object into a JSON object. What happens to the nested objects ?
+let fromJStoJSON = JSON.stringify(marioGame, null, 6)
+console.log(marioGame)
+
+// Convert and pretty print this JS object into a JSON object.
+//  Hint : Check out the JSON lesson on the platform.
+// Use your web inspector to add a breakpoint. 
+// Check the values of the JSON object in the debugger.
